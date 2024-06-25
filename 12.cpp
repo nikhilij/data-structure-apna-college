83674b798a8882ade27d93dc173e634d18ad67dd
nikhilij <roynikhil973@gmail.com>
2024-06-25 18:57:45 +0530

    Added one file

---
+++ 12.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+    string s;
+    getline(cin,s);
+
+    // for(int i=0;i<s.length();i++){
+    //     if(s[i]>=97)
+    //     s[i]=s[i]-32;// this will convert the string to all uppercase
+    // }
+
+
+    // for(int i=0;i<s.length();i++){
+    //     if(s[i]>=65 && s[i]<=91){
+    //         s[i]=s[i]+32; // this will convert the string to all to lowercase
+    //     }
+    // }
+
+    transform(s.begin(),s.end(),s.begin(),::tolower); // this will transform the string into upper or lowercase
+
+    
+
+    cout<<s;
+
+    
+}
---
+++ 13.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void bubblesort(string &s,int n){
+    int swapped;
+    for(int i=0;i<n-1;i++){
+        swapped=0;
+        for(int j=0;j<n-i-1;j++){
+            if(s[j]<s[j+1]){
+                char temp=s[j];
+                s[j]=s[j+1];
+                s[j+1]=temp;
+                swapped=1;
+            }
+        }
+        if(swapped==0){
+            break;
+        }
+    }
+}
+
+
+int main(){
+    string s="123456789";
+
+    // sort(s.begin(),s.end(),greater<int>());
+    // or you can simply use a bubble sort for sorting
+
+    int len=s.length();
+    bubblesort(s,len);
+
+    cout<<s<<endl;
+    cout<<typeid(s).name()<<endl;
+    return 0;
+}
