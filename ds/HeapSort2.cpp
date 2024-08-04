d5f3d66f8b4d3b1821fbde5e91118e630bcef32e
nikhilij <roynikhil973@gmail.com>
2024-08-05 03:08:15 +0530

    added

---
+++ ds/HeapSort2.cpp
@@ -0,0 +1,55 @@
+// wap to heap sort 
+// first we need to heapify the elements
+// in maxHeap and then removing the roots and replacing them with the last element 
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void heapify(vector<int> &a,int n,int i){
+    int largest=i;
+    int left=2*i+1;
+    int right=2*i+2;
+
+    if(left<n && a[left]>a[largest]){
+        largest=left;
+    }
+    if(right<n && a[right]>a[largest]){
+        largest=right;
+    }
+
+    if(largest!=i){ // if element is not root
+        swap(a[i],a[largest]);
+        heapify(a,n,i);
+    }
+}
+
+
+void heapSort(vector<int> &a){
+    int n=a.size();
+
+    for(int i=n/2-1;i>=0;i--){ // this is for building max-heap from the last non leaf node
+
+    heapify(a,n,i);
+
+    }
+
+    // now our max-heap is created and we are intended to remove and heapify to sort the elements 
+
+    for(int i=n-1;i>0;i--){
+        swap(a[0],a[i]);
+        heapify(a,i,0);
+        // here we have taken 0 , as because, max-heap is created already and the 0th index have the largest element 
+    }
+
+}
+
+
+int main(){
+    vector<int> a={12, 11, 13, 5, 6, 7};
+    heapSort(a);
+
+    for(auto it: a){
+        cout<< it  <<" ";
+    }
+}
