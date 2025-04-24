f7b609de100939d2d45025caad678e8de304a8f8
nikhilij <roynikhil973@gmail.com>
2025-04-24 17:29:56 +0530

    --allow-empty-messages

---
+++ ds/PrimsAlgorithm.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <utility>   // for std::pair
+#include <limits>    // for INT_MAX
+#include <tuple>
+
+using namespace std;
+
+int PrimsAlgo(vector<vector<pair<int, int>>> &adj, int V)
+{
+    vector<int> key(V, INT_MAX);
+    vector<bool> inMST(V, false);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+    key[0] = 0;
+    pq.push({0, 0}); // {key, vertex}
+    int mstCost = 0;
+
+    while (!pq.empty())
+    {
+        int u = pq.top().second;
+        pq.pop();
+
+        if (inMST[u])
+            continue;
+
+        inMST[u] = true;
+        mstCost += key[u];
+
+        for (auto &edge : adj[u])
+        {
+            int v = edge.first;
+            int wt = edge.second;
+
+            if (!inMST[v] && wt < key[v])
+            {
+                key[v] = wt;
+                pq.push({key[v], v});
+            }
+        }
+    }
+
+    return mstCost;
+}
+
+int main()
+{
+    int V = 5;
+    vector<vector<pair<int, int>>> adj(V);
+
+    vector<tuple<int, int, int>> edges{
+        {0, 1, 2},
+        {0, 3, 6},
+        {1, 2, 3},
+        {1, 3, 8},
+        {1, 4, 5},
+        {2, 4, 7},
+        {3, 4, 9}};
+
+    for (const auto &edge : edges)
+    {
+        int u, v, w;
+        std::tie(u, v, w) = edge;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w}); // Since it's an undirected graph
+    }
+
+    int mstCost = PrimsAlgo(adj, V);
+    cout << "Total weight of Minimum Spanning Tree: " << mstCost << endl;
+
+    return 0;
+}
