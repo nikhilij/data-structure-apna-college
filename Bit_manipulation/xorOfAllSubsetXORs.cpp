2bb0a9a69b0c14192747638bc8272e27b207607b
nikhilij <roynikhil973@gmail.com>
2025-05-28 11:06:06 +0530

    --allow-empty-messages

---
+++ Bit_manipulation/xorOfAllSubsetXORs.cpp
@@ -0,0 +1,35 @@
+// write the cpp program to find the xor of xor of all subset XORs of an array
+#include <iostream>
+using namespace std;
+
+int xorOfAllSubsetXORs(int arr[], int n)
+{
+    int totalSubsets = 1 << n;
+    int result = 0;
+
+    for (int mask = 0; mask < totalSubsets; mask++)
+    {
+        int subsetXor = 0;
+        cout << "Subset: { ";
+        for (int i = 0; i < n; i++)
+        {
+            if (mask & (1 << i))
+            {
+                subsetXor ^= arr[i];
+                cout << arr[i] << " ";
+            }
+        }
+        cout << "} -> xor = " << subsetXor << endl;
+        result ^= subsetXor;
+    }
+    return result;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = xorOfAllSubsetXORs(arr, n);
+    cout << "XOR of all subset XORs = " << result << endl;
+    return 0;
+}
